da89d593f078ba645dfd1386e1d04cecefb28227
Terry Holmes <tchaser3@outlook.com>
2024-04-20 10:18:32 -0400

    Add project files.

---
+++ webapi/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using webapi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace webapi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        
+
+        User[] users = new User[]
+        {
+            new User { id = 1, name = "Haleemah Redfern", email = "email1@mail.com", phone = "01111111", role = 1},
+            new User { id = 2, name = "Aya Bostock", email = "email2@mail.com", phone = "01111111", role = 1},
+            new User { id = 3, name = "Sohail Perez", email = "email3@mail.com", phone = "01111111", role = 1},
+            new User { id = 4, name = "Merryn Peck", email = "email4@mail.com", phone = "01111111", role = 2},
+            new User { id = 5, name = "Cairon Reynolds", email = "email5@mail.com", phone = "01111111", role = 3}
+        };
+
+        // GET: api/Users
+        //[ResponseType(typeof(IEnumerable<User>))]
+        public IEnumerable<User> Get()
+        {
+            return users;
+        }
+        // GET: api/Users/5
+        public IHttpActionResult Get(int id)
+        {
+            var product = users.FirstOrDefault((p) => p.id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
